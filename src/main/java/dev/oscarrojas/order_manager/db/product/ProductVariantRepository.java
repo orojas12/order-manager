59bcdbe2a524e37675c2a78449c25a7b983b1a02
Oscar Rojas <orojas12@outlook.com>
2026-01-25 21:25:19 -0700

    remove repository annotation causing autowire ambiguity error

--- src/main/java/dev/oscarrojas/order_manager/db/product/ProductVariantRepository.java
+++ src/main/java/dev/oscarrojas/order_manager/db/product/ProductVariantRepository.java
@@ -1,11 +1,9 @@
 package dev.oscarrojas.order_manager.db.product;
 
 import dev.oscarrojas.order_manager.product.ProductVariantModel;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface ProductVariantRepository {
 
     Optional<ProductVariantModel> get(String variantId);
