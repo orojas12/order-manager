31bec36dacbc1597191d1dde076430ed965d2630
Oscar Rojas <orojas12@outlook.com>
2026-01-25 17:47:17 -0700

    add temporary ProductVariantRepository implementation to stop autowire errors

---
+++ src/main/java/dev/oscarrojas/order_manager/db/product/ProductVariantRepositoryInMemory.java
@@ -0,0 +1,18 @@
+package dev.oscarrojas.order_manager.db.product;
+
+import dev.oscarrojas.order_manager.product.ProductVariantModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class ProductVariantRepositoryInMemory implements ProductVariantRepository {
+
+    @Override
+    public Optional<ProductVariantModel> get(String variantId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void create(ProductVariantModel variant) {}
+}
