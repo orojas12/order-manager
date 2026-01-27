d130b967d6f9167ed403b82c6983103393aa49b0
Oscar Rojas <orojas12@outlook.com>
2026-01-26 22:15:45 -0700

    add productId to variant response

--- src/main/java/dev/oscarrojas/order_manager/product/ProductService.java
+++ src/main/java/dev/oscarrojas/order_manager/product/ProductService.java
@@ -3,6 +3,7 @@ package dev.oscarrojas.order_manager.product;
 import dev.oscarrojas.order_manager.exception.ValidationException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -10,10 +11,12 @@ import java.util.UUID;
 public class ProductService {
 
     private final ProductRepository repository;
+    private final ProductMapper mapper;
     private final CreateProductValidator validator;
 
-    public ProductService(ProductRepository repository) {
+    public ProductService(ProductRepository repository, ProductMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
         this.validator = new CreateProductValidator();
     }
 
@@ -37,10 +40,15 @@ public class ProductService {
         repository.create(product);
 
         List<ProductVariantResponse> variantResponses = variants.stream()
-                .map(variant -> new ProductVariantResponse(variant.id(), variant.sku(), variant.attributes()))
+                .map(variant -> new ProductVariantResponse(
+                        variant.id(), variant.sku(), variant.attributes(), variant.productId()))
                 .toList();
 
         return new ProductResponse(
                 product.id(), product.brand(), product.name(), product.description(), variantResponses);
+    }
+
+    public Collection<ProductResponse> getProductList() {
+        return repository.getAll().stream().map(mapper::toResponse).toList();
     }
 }
